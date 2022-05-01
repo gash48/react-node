336514cd1f797284ad48effca221213c95cd36f0
Gash <gangster.thesecretgun@gmail.com>
2022-05-01 14:55:11 +0530

    app and server url

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const SERVER_URL = process.env.SERVER_URL || process.env.REACT_APP_SERVER_URL;
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 console.log(SERVER_URL);
 
 axios.defaults.baseURL = SERVER_URL + "/";
--- src/services/stripe.js
+++ src/services/stripe.js
@@ -3,5 +3,5 @@ import { loadStripe } from "@stripe/stripe-js";
 export default () =>
   loadStripe(
     // "pk_test_51Kr380SEtDJPIfgGgiAS2BitW9vBM5cW3Xx1X8UUEQcWSgdgE4XdGEgAP33p5f6xcklBzk4ShUkCYKQMN11VRS9I00oamwNdwh"
-    process.env.STRIPE_KEY
+    process.env.REACT_APP_STRIPE_KEY
   );
